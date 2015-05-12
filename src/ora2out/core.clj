eaffa16e9c9e246653e9c6560c3c8ce7088d99f4
Matt Pettis <matthew.pettis@gmail.com>
2015-05-11 22:54:55 -0500

    levelset -- done for night.

--- src/ora2out/core.clj
+++ src/ora2out/core.clj
@@ -1,24 +1,27 @@
 (ns ora2out.core
-  ;(:require [clojure.java.jdbc :as jdbc]) 
-  ;(use clojure.java.jdbc)
+  (require [clojure.java.jdbc :as j] [clojure.data.csv :as csv] [clojure.java.io :as io])
   (:gen-class))
+;(require '[clojure.java.jdbc :as j] '[clojure.data.csv :as csv] '[clojure.java.io :as io])
 
 ; https://github.com/clojure/java.jdbc
 ; http://clojure-doc.org/articles/ecosystem/java_jdbc/home.html
 ; https://github.com/clojure/data.csv
 ; http://clojure-doc.org/articles/ecosystem/java_jdbc/using_sql.html
-(require '[clojure.java.jdbc :as j] '[clojure.data.csv :as csv] '[clojure.java.io :as io])
 
-(def db {:classname "oracle.jdbc.OracleDriver"
-           :subprotocol "oracle"
-           :subname "thin:@tcp-science1.quantumretail.com:1521:quantum" 
-           :user "quantum"
-           :password "Q2010"})
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!")
+  ; http://stackoverflow.com/questions/14322839/clojure-string-format-s-a-fails-with-classcastexception
+  (println  (format "%s" "a word"))
+
+  ; Database connection definition
+  (def db {:classname "oracle.jdbc.OracleDriver"
+             :subprotocol "oracle"
+             :subname "thin:@tcp-science1.quantumretail.com:1521:quantum" 
+             :user "quantum"
+             :password "Q2010"})
 
   ;(println (j/query db ["select * from qr_system_config"] :row-fn :current_run_cycle))
   ;(j/query db ["select * from qr_calendar_454 where week_begin_dt = '02-AUG-15'"] :row-fn #'println)
