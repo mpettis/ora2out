55673d5d81aca8dbfbd386994e8a66f770fa4554
Matt Pettis <matthew.pettis@gmail.com>
2015-05-19 20:38:52 -0500

    This version will print hardcoded query to standard out, and take configurations as arguments

--- src/ora2out/core.clj
+++ src/ora2out/core.clj
@@ -1,5 +1,9 @@
 (ns ora2out.core
-  (require [clojure.java.jdbc :as j] [clojure.data.csv :as csv] [clojure.java.io :as io])
+  (require [clojure.java.jdbc :as j]
+           [clojure.data.csv :as csv]
+           [clojure.java.io :as io]
+           [clojure.pprint :as pp]
+           [clojure.tools.cli :refer [parse-opts] :as c])
   (:gen-class))
 ;(require '[clojure.java.jdbc :as j] '[clojure.data.csv :as csv] '[clojure.java.io :as io])
 
@@ -10,18 +14,34 @@
 
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Take a query and database connection string info and return query as CSV output"
   [& args]
-  (println "Hello, World!")
+  ;(println "Hello, World!")
   ; http://stackoverflow.com/questions/14322839/clojure-string-format-s-a-fails-with-classcastexception
-  (println  (format "%s" "a word"))
+  ;(println  (format "%s" "a word"))
+
+  ; parse command line options
+  ; http://blog.cymen.org/2012/03/09/clojure-using-clojuretools-cli-to-parse-command-line-arguments/
+  (let [[options args banner]
+          (c/cli args
+            ["-host" "Database host" :default "tcp-science1.quantumretail.com"]
+            ["-sid" "Database SID" :default "quantum"]
+            ["-port" "Database port to listen on" :default "1521"]
+            ["-user" "Database user" :default "quantum"]
+            ["-password" "Database password" :default "Q2010"])]
+      ;(println "port:" (:port options))
+      (def argopts options))
+
+    ;(println "port:" (:port argopts))
 
   ; Database connection definition
   (def db {:classname "oracle.jdbc.OracleDriver"
              :subprotocol "oracle"
-             :subname "thin:@tcp-science1.quantumretail.com:1521:quantum" 
-             :user "quantum"
-             :password "Q2010"})
+             ;:subname "thin:@tcp-science1.quantumretail.com:1521:quantum" 
+             :subname (format "thin:@%s:%s:%s" (:host argopts) (:port argopts) (:sid argopts)) 
+             :user (:user argopts)
+             :password (:password argopts)})
+  ;(pp/pprint db)
 
   ;(println (j/query db ["select * from qr_system_config"] :row-fn :current_run_cycle))
   ;(j/query db ["select * from qr_calendar_454 where week_begin_dt = '02-AUG-15'"] :row-fn #'println)
@@ -48,17 +68,31 @@
 
 
   ;(def q ["select week_begin_dt, year_nbr from qr_calendar_454 where week_begin_dt = '02-AUG-15'"])
-  (def q ["select * from db_config_param where name = 'SELECT.RC_POUTL'"])
-  ;(def rs (j/query db q))
-  (def rs (j/query db q :as-arrays? true))
-  (println rs)
+
+  ;(def q ["select * from db_config_param where name = 'SELECT.RC_POUTL'"])
+  ;;(def rs (j/query db q))
+  ;(def rs (j/query db q :as-arrays? true))
+  ;;(println rs)
 
 
   ;(with-open [file (io/writer "/tmp/results.csv")]
       ;(csv/write-csv file rs))
-    (let [header (map name (first rs))]
-      (with-open [file (io/writer "/tmp/results.csv")]
-          (csv/write-csv file (cons header (rest rs)))))
 
-    (println "End!"))
+      ; write to file
+  ;(def q ["select * from db_config_param where name = 'SELECT.RC_POUTL'"])
+  ;(def rs (j/query db q :as-arrays? true))
+  ;;(println rs)
+  ;(let [header (map name (first rs))]
+    ;(with-open [file (io/writer "/tmp/results.csv")]
+        ;(csv/write-csv file (cons header (rest rs)))))
+
+    ;; Write to stdout
+  ;(def q ["select * from db_config_param where name = 'SELECT.RC_POUTL'"])
+  (def q ["select * from qr_graph"])
+  (def rs (j/query db q :as-arrays? true))
+  (let [header (map name (first rs))]
+    (csv/write-csv *out* (cons header (rest rs))))
+
+    ;(println "End!")
+    )
 
